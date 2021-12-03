91a653b127da0263207dd09bf5f7852a0eaa2ccd
Andrew Blanchette <andrew@Andrews-MacBook-Air.local>
2021-12-03 17:30:20 -0500

    Completed Part 1

---
+++ Day1 Part1/Program.cs
@@ -0,0 +1,33 @@
+﻿int[] numbers = new int[2000];
+
+void PopulateInput()
+{
+    int i= 0; 
+
+    foreach (string line in System.IO.File.ReadLines("input.txt"))
+    {
+        numbers.SetValue(Int32.Parse(line), i);
+        i++;
+    }
+}
+
+int GetIncreases()
+{
+    int previous_depth = 0;
+    int current_depth = 1;
+    int increase_counter = 0;
+
+    while (current_depth <= numbers.Length - 1)
+    {
+        if (numbers[previous_depth] < numbers[current_depth])
+        {
+            increase_counter++;
+        }
+        previous_depth++;
+        current_depth++;
+    }
+    return increase_counter;
+}
+
+PopulateInput();
+Console.WriteLine(GetIncreases());
